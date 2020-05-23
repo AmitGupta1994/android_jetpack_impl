d187b1d56f9ea901a852aa4fceaacafd577d7d54
Amit Gupta <iamitgupta1994@gmail.com>
2020-05-23 18:52:15 +0545

    [add][bottom_nav_view_nav_comp]: connects action bar with navigation component in LoginActivity

--- app/src/main/java/com/amitgupta/android_jetpack_impl/LoginActivity.kt
+++ app/src/main/java/com/amitgupta/android_jetpack_impl/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.amitgupta.android_jetpack_impl
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 
 class LoginActivity : AppCompatActivity(){
 
@@ -10,5 +12,9 @@ class LoginActivity : AppCompatActivity(){
 
         setContentView(R.layout.activity_login)
 
+        // implementing action bar changes
+        val navController = Navigation.findNavController(this,R.id.nav_host_fragment)
+
+        NavigationUI.setupActionBarWithNavController(this,navController)
     }
 }
