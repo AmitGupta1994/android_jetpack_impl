7ecc8af461e2c41f994ba84fa47d968ad50e2d32
Amit Gupta <iamitgupta1994@gmail.com>
2020-05-23 18:37:04 +0545

    [add][bottom_nav_view_with_nav_comp]: adds flow from signup fragment to profile fragment in navigation_resource_file

--- app/src/main/java/com/amitgupta/android_jetpack_impl/fragment/ProfileFragment.kt
+++ app/src/main/java/com/amitgupta/android_jetpack_impl/fragment/ProfileFragment.kt
@@ -23,6 +23,5 @@ class ProfileFragment : Fragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Toast.makeText(activity,arguments?.get("message") as String, Toast.LENGTH_SHORT).show()
     }
 }
